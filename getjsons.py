07a3871694ffff7c8d18f64d06a49fa60cfe9a37
josephabonasara <jabon028@uottawa.ca>
2020-09-20 17:43:49 -0400

    s

--- getjsons.py
+++ getjsons.py
@@ -3,8 +3,8 @@ def getjson(keyword,website):
     filename = 'data.json'
     with open(filename, 'r') as f:
         data = json.load(f)
-        data.append((str(keyword),str(website))) # tupleeee
-
+       # data.append((str(keyword),str(website))) # tupleeee
+        data[str(keyword)]=str(website)
     os.remove(filename)
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4)
